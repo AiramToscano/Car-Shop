8992cbdf288543a524d023991af91b97bd631aa3
Airam Toscano <airamlobato@gmail.com>
2022-07-29 19:50:45 -0400

    Feat: add required zodSchema

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -2,8 +2,12 @@ import { z } from 'zod';
 import { IVehicle } from './IVehicle';
 
 const ICarZodSchema = z.object({
-  doorsQty: z.number().min(2).max(4),
-  seatsQty: z.number().min(2).max(7),
+  model: z.string().nonempty(),
+  year: z.number().min(1900).max(2022),
+  color: z.string().nonempty(),
+  buyValue: z.number().int(),
+  doorsQty: z.number().min(2).max(4).int(),
+  seatsQty: z.number().min(2).max(7).int(),
 });
 
 export type ICar = IVehicle & z.infer<typeof ICarZodSchema>;
