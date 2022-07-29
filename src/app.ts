003d106392990f676869f803308ff01dc9189f94
Airam Toscano <airamlobato@gmail.com>
2022-07-29 19:48:23 -0400

    Feat: Carroute and express.json

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/Error';
+import CarRouter from './routes/ICarRoute';
 
 const app = express();
+app.use(express.json());
+app.use(CarRouter);
+app.use(errorHandler);
 
 export default app;
