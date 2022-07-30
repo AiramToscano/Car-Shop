447f1be3cd50834203f51923f3bbb96b1de59520
Airam Toscano <airamlobato@gmail.com>
2022-07-30 19:27:44 -0400

    Feat: add function delete and update

--- src/interfaces/IService.ts
+++ src/interfaces/IService.ts
@@ -2,7 +2,8 @@ interface IService<T> {
   create(obj:T):Promise<T>,
   read():Promise<T[]>,
   readOne(_id:string):Promise<T>
-  // update(_id: string, body: T): Promise<T | null>,
+  update(_id: string, body: T): Promise<T | null>,
+  delete(_id:string): Promise<T | null>,
 }
 
 export default IService;
