ef4baea09e8c67a647fac3e62697ce70803de988
Airam Toscano <airamlobato@gmail.com>
2022-07-30 17:42:45 -0400

    Feat: add route get/cars and get/cars/:id

--- src/routes/ICarRoute.ts
+++ src/routes/ICarRoute.ts
@@ -10,5 +10,7 @@ const CarService = new ICarService(Car);
 const CarController = new ICarController(CarService);
 
 route.post('/cars', (req, res) => CarController.create(req, res));
+route.get('/cars', (req, res) => CarController.read(req, res));
+route.get('/cars/:id', (req, res) => CarController.readOne(req, res));
 
 export default route;
