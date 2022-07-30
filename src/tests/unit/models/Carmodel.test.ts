f620774826d0f347346ffd3824233c4685ee1b33
Airam Toscano <airamlobato@gmail.com>
2022-07-30 19:36:19 -0400

    Test: FindOne test coverage

--- src/tests/unit/models/Carmodel.test.ts
+++ src/tests/unit/models/Carmodel.test.ts
@@ -7,11 +7,12 @@ import {
 	CarMockWithId,
 } from '../../mocks/CarMoks';
 
-describe('Frame Model', () => {
+describe('Car Model', () => {
 	const carModel = new CarModel();
 
 	before(() => {
 		sinon.stub(Model, 'create').resolves(CarMockWithId);
+		sinon.stub(Model, 'findOne').resolves(CarMockWithId);
 	});
 
 	after(() => {
@@ -22,6 +23,21 @@ describe('Frame Model', () => {
 		it('successfully created', async () => {
 			const carFrame = await carModel.create(CarMock);
 			expect(carFrame).to.be.deep.equal(CarMockWithId);
+		});
+	});
+
+	describe('searching a car', () => {
+		it('successfully found', async () => {
+			const carFound = await carModel.readOne('62cf1fc6498565d94eba52cd');
+			expect(carFound).to.be.deep.equal(CarMockWithId);
+		});
+
+		it('_id not found', async () => {
+			try {
+				await carModel.readOne('123ERRADO');
+			} catch (error: any) {
+				expect(error.message).to.be.eq('InvalidMongoId');
+			}
 		});
 	});
 });
