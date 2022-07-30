59d548ce42b66254e87e9df56fb052503b17d577
Airam Toscano <airamlobato@gmail.com>
2022-07-30 17:43:16 -0400

    Feat: create read() and readOne()

--- src/services/CarsService.ts
+++ src/services/CarsService.ts
@@ -1,11 +1,12 @@
 import IService from '../interfaces/IService';
 import { ICar, ICarZodSchema } from '../interfaces/ICar';
 import { IModel } from '../interfaces/IModel';
+import { ErrorTypes } from '../errors/catolog';
 
 class FrameService implements IService<ICar> { 
-  private _frame:IModel<ICar>;
+  private _car:IModel<ICar>;
   constructor(model:IModel<ICar>) {
-    this._frame = model;
+    this._car = model;
   }
 
   public async create(obj:ICar):Promise<ICar> {
@@ -13,7 +14,17 @@ class FrameService implements IService<ICar> {
     if (!parsed.success) {
       throw parsed.error;
     }
-    return this._frame.create(obj);
+    return this._car.create(obj);
+  }
+
+  public async read():Promise<ICar[]> {
+    return this._car.read();
+  }
+
+  public async readOne(_id:string):Promise<ICar> {
+    const frame = await this._car.readOne(_id);
+    if (!frame) throw new Error(ErrorTypes.EntityNotFound);
+    return frame;
   }
 }
 
